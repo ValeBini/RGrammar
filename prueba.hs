b40f30ce39823de7045e4c5ad62f088e2539dd29
ValeBini <valentinabini98@gmail.com>
2020-01-11 11:41:47 -0300

    compila el dfaF hay que terminar el nfaToDFA

--- prueba.hs
+++ prueba.hs
@@ -1,5 +1,5 @@
-import Data.List as L
-import Data.Set as S
+import Data.Set (fromList, isSubsetOf, intersection, empty)
+import Data.List (union)
 
 newtype T = T {runT :: String}
     deriving (Eq, Ord, Show)
@@ -32,33 +32,54 @@ data NFA a = NA [Sym] [State a] (R a) [State a] (State a)
 
 gramToNFA :: Gram -> NFA (Maybe String)
 gramToNFA (G nts ts ps nt) = NA syms states r ac i
-  where syms = L.map (\t -> Sym (Just (runT t))) ts
-        states = ((State Nothing):(L.map (\nt -> State (Just (runNT nt))) nts))
-        r =  R (L.union [(State (Just (runNT s)), Sym (Just (runT x)), State (Just (runNT b))) | s<-nts, x<-ts, b<-nts, elem (PN s x b) ps]
+  where syms = map (\t -> Sym (Just (runT t))) ts
+        states = ((State Nothing):(map (\nt -> State (Just (runNT nt))) nts))
+        r =  R (union [(State (Just (runNT s)), Sym (Just (runT x)), State (Just (runNT b))) | s<-nts, x<-ts, b<-nts, elem (PN s x b) ps]
                         [(State (Just (runNT s)), Sym (Just (runT x)), State Nothing) | s<-nts, x<-ts, elem (PT s x) ps])
         ac = (State Nothing):[State (Just (runNT s)) | s<-nts, elem (PE s) ps]
         i = (State (Just (runNT nt)))
 
-
+{-}
 included :: [State [Maybe String]] -> [State [Maybe String]] -> Bool
-included xs ys = let xs' = L.map (\x -> fromList (runState x)) xs
-                     ys' = L.map (\y -> fromList (runState y)) ys
+included xs ys = let xs' = map (\x -> fromList (runState x)) xs
+                     ys' = map (\y -> fromList (runState y)) ys
                  in isSubsetOf (fromList xs') (fromList ys')
+-}
+
+included :: F [Maybe String] -> F [Maybe String] -> Bool
+included (F xs) (F ys) = let xs' = map (\(s1, s, s2) -> (fromList (runState s1), s, fromList (runState s2))) xs
+                             ys' = map (\(s1, s, s2) -> (fromList (runState s1), s, fromList (runState s2))) ys
+                         in isSubsetOf (fromList xs') (fromList ys')
+
+equal :: [State [Maybe String]] -> [State [Maybe String]] -> Bool
+equal xs ys = let xs' = map (\x -> fromList (runState x)) xs
+                  ys' = map (\y -> fromList (runState y)) ys
+              in (fromList xs') == (fromList ys')
 
 
-dfaF :: [Sym] -> R -> [State [Maybe String]] -> (F, [States [Maybe String]])
-dfaF sx sr (F fall) (F flast) = let f = (concat (L.map (\(s1, x1, s1') -> L.map (\x -> (s1', x, t s1' x)) sx) flast))
-                    in if included f fall then (F fall) else F (dfaF sx sr (F (L.union fall f)) (F f))
-
-
-
+dfaF :: [Sym] -> R (Maybe String) -> [State (Maybe String)] -> F [Maybe String] -> F [Maybe String] -> F [Maybe String]
+dfaF xs (R rs) sts (F fall) (F flast) = let f = concat (map (\(s1, x1, s1') -> map (\x -> (s1', x, t s1' x)) xs) flast)
+                                        in if included (F f) (F fall) then (F fall) else (dfaF xs (R rs) sts (F (union fall f)) (F f))
+        where t st x = State (concat (map (\s -> [runState s' | s'<-sts, elem (State s, x, s') rs]) (runState st)))
 
 nfaToDFA :: NFA (Maybe String) -> DFA [Maybe String]
-nfaToDFA (NA xs st (R rs) ac i) = DA xs st' f ac' i'
-    where i' = State (L.union [runState i] [runState s | s<-st, elem (i, Sym Nothing, s) rs])
+nfaToDFA (NA xs st rs ac i) = DA xs st' f ac' i'
+    where i' = State (union [runState i] [runState s | s<-st, elem (i, Sym Nothing, s) rs])
+          f = dfaF xs rs st (F [])
           st' = dfaStates xs rs [i']
-          dfaStates sx sr sst = let sst' = L.union sst (concat (L.map (\s -> L.map (\x -> t s x) sx) sst))
+          dfaStates sx sr sst = let sst' = union sst (concat (map (\s -> map (\x -> t s x) sx) sst))
                                  in if (equal sst sst') then sst else dfaStates sx sr sst'
-          t sts x = concat (L.map (\s -> State [s' | s'<-st', elem (State s, x, State s') rs]) (runState sts))
+          t sts x = concat (map (\s -> State [s' | s'<-st', elem (State s, x, State s') rs]) (runState sts))
+          ac' = [s | s<-st', (intersection (fromList (runState s)) (fromList (runState ac))) /= empty]
+
+{-}
+nfaToDFA :: NFA (Maybe String) -> DFA [Maybe String]
+nfaToDFA (NA xs st (R rs) ac i) = DA xs st' f ac' i'
+    where i' = State (union [runState i] [runState s | s<-st, elem (i, Sym Nothing, s) rs])
+          st' = dfaStates xs rs [i']
+          dfaStates sx sr sst = let sst' = union sst (concat (map (\s -> map (\x -> t s x) sx) sst))
+                                 in if (equal sst sst') then sst else dfaStates sx sr sst'
+          t sts x = concat (map (\s -> State [s' | s'<-st', elem (State s, x, State s') rs]) (runState sts))
           f = F [(s, x, t s x) | s<-st', x<-xs]
-          ac' = [s | s<-st', (S.intersection (fromList (runState s)) (fromList (runState ac))) /= S.empty]
+          ac' = [s | s<-st', (intersection (fromList (runState s)) (fromList (runState ac))) /= empty]
+-}
